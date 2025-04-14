698441f08d1df3eae20bd25fa54a0d385ad1745c
Doreamonpfp <doreamonpfp@gmail.com>
2025-04-14 14:45:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ addEventListener('fetch', event => {
 });
 
 // Add your bot token here.
-const BOT_TOKEN = "bot token here";
+const BOT_TOKEN = "7683979215:AAHJnjWokkweyEq7lVakopOIa587P_JiWL8";
 
 //Pro people: don't change this else your bot will be fucked.
 const API_BASE_URL = "https://probable-berti-frozenbotspvt-17e82b7b.koyeb.app";
